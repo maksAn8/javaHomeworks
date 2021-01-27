df38ceac709917169f325c6ffa78b472ee78a4b6
Maksym Anatskiy <maksan1997@gmail.com>
2021-01-27 17:32:47 +0200

    Update ConditionalStatements.java

--- ConditionalStatements.java
+++ ConditionalStatements.java
@@ -1,43 +1,47 @@
 public class ConditionalStatemants {
 
-	public int sumOrMultiply(int a, int b) {
+	public int getSumOrProduct(int a, int b) {
 		return (a&1) != 1 ? a*b : a+b;
 	}
 	
-	public String pointPosition(int x, int y) {
+	public String getQuadrantByCoordinates(int x, int y) {
 		if(x>0) {
-			if(y>0) return "1-я четверть";
-			else if(y<0) return "4-я четверть";
-			else return"Точка на положительной части оси х";
+			if(y>0) return "The first quadrant.";
+			else if(y<0) return "The fourth quadrant.";
+			else return"The point is on the X axis between I and IV quadrants.";
 		} else if(x<0) {
-			if(y>0) return "2-я четверть";
-			else if(y<0) return "3-я четверть";
-			else return "Точка на отрицательной части оси х";
+			if(y>0) return "The second quadrant.";
+			else if(y<0) return "The third quadrant.";
+			else return "The point is on the X axis between II and III quadrants.";
 		}
-		if(y>0) return "Точка на положительной части оси y";
-		if(y<0) return "Точка на отрицательной части оси y";
-		return "Начало координат";
+		if(y>0) return "The point is on the Y axis between I and II quadrants.";
+		if(y<0) return "The point is on the Y axis between III and IV quadrants.";
+		return "The origin.";
 	}
 	
-	public int positiveNumbersSum(int a, int b, int c) {
-		if(a<0) a=0;
-		if(b<0) b=0;
-		if(c<0) c=0;
-		return a+b+c;
+	public int getPositiveNumbersSum(int a, int b, int c) {
+		int result = 0;
+		if(a>0) result += a;
+		if(b>0) result += b;
+		if(c>0) result += c;
+		return result;
 	}
 	
-	public int maxSumOrProduct(int a, int b, int c) {
+	public int getMaxBetweenSumAndProduct(int a, int b, int c) {
 		int sum = a+b+c, multiply = a*b*c;
-		if(sum>multiply) return sum+3;
-		else return multiply+3;
+		if(sum > multiply) return sum + 3;
+		else return multiply + 3;
 	}
 	
-	public char getGrade(int grade) {
-		if(grade>=0 && grade<=19) return 'F';
-		else if(grade>=20 && grade<=39) return 'E';
-		else if(grade>=40 && grade<=59) return 'D';
-		else if(grade>=60 && grade<=74) return 'C';
-		else if(grade>=75 && grade<=89) return 'B';
+	public char getGradeByRating(int rating) {
+		if(rating < 0 || rating > 100) {
+			throw new IllegalArgumentException("The rating must be in the range of 0-100!");
+		}
+		if(rating >=0 && rating <= 19) return 'F';
+		else if(rating <= 39) return 'E';
+		else if(rating <= 59) return 'D';
+		else if(rating <= 74) return 'C';
+		else if(rating <= 89) return 'B';
 		return 'A';
 	}
 	
