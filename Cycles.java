fd65d9e0b2368c850f154d312b304c8b385cbc2c
Maksym Anatskiy <maksan1997@gmail.com>
2021-01-31 22:48:51 +0200

    Update Cycles.java

--- Cycles.java
+++ Cycles.java
@@ -1,104 +1,99 @@
-import java.util.Random;
 
 public class Cycles {
-
 	
-	public static void main(String args[]) {
-		Random random = new Random();
-		int a = random.nextInt(100)+1;
-		Homework_1_2 h = new Homework_1_2();
-		System.out.println("Исходные данные "+a);
-		h.task_1();
-		h.task_2(a);
-		h.task_3(a);
-		h.task_5(a);
-		h.task_6(a);
-		a = random.nextInt(6);
-		h.task_4(a);
+	public static void getEvenNumbersSumAndQuantity() {
+		int numCounter = 0, numSum = 0, upperLimit = 98, lowerLimit = 2;
+		for(int i=lowerLimit; i<=upperLimit; i+=2) {
+			numCounter++;
+			numSum+=i;
+		}
+		System.out.println("Even numbers quantity = "+numCounter+", their sum = "+numSum);	
 	}
 	
-	public void task_1() {
-		int numCounter=0, numSum=0;
-		for(int i=1; i<100; i++) {
-			if((i&1)!=1) {
-				numCounter++;
-				numSum=numSum+i;
+	public static boolean isPrimeNumber(int number) {
+		if(number<=1) return false;
+		for(int i=2; i<number; i++) {
+			if(number%i==0) return false;
+		}
+		return true;	
+	}
+	
+	public static void getSqrtWithPrecisionToInteger(int number) {
+		
+		if(number<0)
+			throw new IllegalArgumentException("Number must be in the range of 0-2147483647");
+		
+		if(number<2) {
+			System.out.println("SQRT = "+number);
+			return;
+		}
+		
+		if(number>=2147395600) {
+			System.out.println("SQRT = 46340");
+			return;
+		}
+			
+		//selection method
+		for(int i=1; i<=number/2; i++) {
+			if(i*i<=number && (i+1)*(i+1)>number) {
+				System.out.println("SQRT = "+i);
+				break;
 			}
 		}
-		System.out.println("Кол-во чётных чисел = "+numCounter+", их сумма = "+numSum);	
-	}
-	
-	public void task_2(int a) {
-		for(int i=2; i<=a; i++) {
-			if(a%i==0 && i!=a) {
-				System.out.println("Число составное");
+		
+		//binary search
+		System.out.println("Binary search:");
+		int sqrt, leftBoundary=1, rightBoundary=number/2;
+		
+		if(rightBoundary>46340) rightBoundary=46340;
+		
+		while(leftBoundary<=rightBoundary) {
+			sqrt = (leftBoundary+rightBoundary)/2;
+			if(sqrt*sqrt==number) {
+				System.out.println("SQRT = "+sqrt);
 				return;
+			} else if(sqrt*sqrt>number){
+				rightBoundary=sqrt;
+			} else if(sqrt*sqrt<number) {
+				if(++sqrt*sqrt>number) {
+					System.out.println("SQRT = "+--sqrt);
+					return;
+				} else {
+					leftBoundary=sqrt;
+				}
 			}
-		}
-		System.out.println("Число простое");	
-	}
-	
-	public void task_3(int a) {
-		for(int i=1; i<=a/2; i++) {
-			if(i*i==a) {
-				System.out.println("Корень = "+i);
-				break;
-			}
-			else if(i*i<a && (i+1)*(i+1)>a) {
-				System.out.println("Корень = "+i);
-				break;
-			}
-		}
-		
-		//бинарный поиск
-		
-		System.out.println("Бинарный поиск:");
-		int firstElement=1;
-		int lastElement=a/2;
-		if(lastElement==0) System.out.println("Корень = "+a);
-		if(lastElement*lastElement==a) System.out.println("Корень = "+lastElement);
-		while(firstElement<=lastElement) {
-			int middleElement=(firstElement+lastElement)/2;
-			if(middleElement*middleElement==a || 
-					middleElement*middleElement<a && (middleElement+1)*(middleElement+1)>a) {
-				System.out.println("Корень = "+middleElement);
-				break;
-			}
-			if(middleElement*middleElement>a) lastElement=middleElement;
-			if(middleElement*middleElement<a) firstElement=middleElement;
 		}
 	}
 	
-	public int task_4(int n) {
-		int f = n;
-		int i = 1;
-		do {
-			f=f*(n-i);	
-			i++;
-		} while(i!=n);
-		System.out.println(n+"! = "+ f);
-		return f;
+	public static int getFactorial(int number) {
+		if(number<0)
+			throw new IllegalArgumentException("Factorial can be found only for positive integers and zero!");
+		if(number==0) return 1;
+		int factorial = number;
+		while(number>2) factorial *= --number;			
+		return factorial;
 	}
 	
-	public int task_5(int a) {
-		int sum=0;
-		int d = a;
-		while(a!=0) {
-			sum=sum+(a%10);
-			a=a/10;
+	public static int getDigitsSum(int number) {
+		int sum = 0;
+		while(number!=0) {
+			sum += (number%10);
+			number/=10;
 		}
-		System.out.println("Сумма цифр числа "+ d +" = "+ sum);
 		return sum;
 	}
 	
-	public void task_6(int a) {
-		System.out.print("Зеркальное отображение числа:");
-		while(a!=0) {
-			int d=(a%10);
-			System.out.print(d);
-			a=a/10;
+	public static String getMirroredNumber(int number) {
+		String mirroredNumber="";
+		if(number<0) {
+			mirroredNumber = "-";
+			number*=-1;
 		}
-		System.out.println("");
+		while(number!=0) {
+			mirroredNumber += number%10;
+			number/=10;
+		}
+		return mirroredNumber;
 	}
 	
 }
