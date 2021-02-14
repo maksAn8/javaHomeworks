a5a7ea7d59c4d44275b1c27c9eb4695fd1485175
Maksym Anatskiy <maksan1997@gmail.com>
2021-02-14 14:27:50 +0200

    Update GuessNumber.java

--- GuessNumber.java
+++ GuessNumber.java
@@ -2,25 +2,50 @@ import java.util.Scanner;
 
 public class GuessNumber {
 
-	public int generatedNumber, attemptsLeft, difference;
-	public int upperLimit, lowerLimit, userGuess, userTry;
-	public Scanner scanner;
+	private int generatedNumber, attemptsLeft, difference;
+	private int upperLimit, lowerLimit, userGuess, userTry;
+	private static int defaultUpperLimit = 100, defaultLowerLimit = 1, defaultAttempts = 5;
+	private static int maxLimit = 200, minLimit = 1, minAttempts = 1, maxAttempts = 15;
+	private Scanner scanner;
 	
 	public GuessNumber() {
-		setUserRange();
-		setUserAttempts();
+		this(defaultLowerLimit, defaultUpperLimit, defaultAttempts); //default settings for game
+	}
+	
+	public GuessNumber(int lowerLimit, int upperLimit, int attempts) {
+		scanner = new Scanner(System.in);
+		if(isValidSettings(lowerLimit, minLimit, maxLimit)) {
+			this.lowerLimit = lowerLimit;
+		} else {
+			this.lowerLimit = defaultLowerLimit;
+		}
+		if(isValidSettings(upperLimit, minLimit, maxLimit)) {
+			this.upperLimit = upperLimit;
+		} else {
+			this.upperLimit = defaultUpperLimit;
+		}
+		if(isValidSettings(attempts, minAttempts, maxAttempts)) {
+			attemptsLeft = attempts;
+		} else {
+			attemptsLeft = defaultAttempts;
+		}
+		
 	}
 	
 	public void start() {
 		generateNumber();
 		while(attemptsLeft != 0) {
-			userGuess = scanner.nextInt();
+			userGuess = getUserGuess();
 			if(userGuess == generatedNumber) {
 				System.out.println(String.format("Congratulations! You guessed the number in %s attempt(s)", ++userTry));
+				exit();
 			} else if(userTry == 0) {
 				userTry++;
 				difference = Math.abs(generatedNumber - userGuess);
 				System.out.println(String.format("Try again! %s attempt(s) left", --attemptsLeft));
+			} else if(attemptsLeft == 1) {
+				attemptsLeft--;
+				System.out.println("Game over!");
 			} else if(Math.abs(generatedNumber - userGuess) < difference) {
 				userTry++;
 				difference = Math.abs(generatedNumber - userGuess);
@@ -35,36 +60,52 @@ public class GuessNumber {
 			} 
 		}
 		System.out.println("It was the number " + generatedNumber);
+		exit();
 	}
 	
-	public void setUserRange() {
-		scanner = new Scanner(System.in);
-		System.out.println("Please, set lower and upper limits of the range to start a game!");
-		lowerLimit = getLowerLimit(); //add validation, i.e. isValidRange(), min = 1 , max = 200
-		upperLimit = getUpperLimit();
-	}
-	
-	public int getLowerLimit() {
-		return scanner.nextInt();
-	}
-	
-	public int getUpperLimit() {
-		return scanner.nextInt();
-	}
-	
-	public boolean isValidRange() {
-		return true;
-	}
-	
-	public void setUserAttempts() {	//add validation, attempts can be from 1 to 15;
-		System.out.println("Please, set amount of attempts from 1 to 15)!");
-		attemptsLeft = scanner.nextInt();
-	}
-	
-	public void generateNumber() {
+	private void generateNumber() {
 		generatedNumber = lowerLimit + (int)(Math.random() * (upperLimit - lowerLimit + 1));
-		System.out.println(String.format("Hi, I thought of a number from %s to %s of your range. Try to guess it in %s attempt(s)!",
+		System.out.println(String.format("Hi, I thought of a number from %s to %s. Try to guess it in %s attempt(s)!",
 				lowerLimit, upperLimit, attemptsLeft));
 	}
 	
+	private int getUserGuess() {
+		int userGuess = 0;
+		boolean isValid = false;
+		while(!isValid) {
+			while(!scanner.hasNextInt()) {
+				if(scanner.next().toLowerCase().equals("exit")) {
+					exit();
+				} else {
+					System.out.println("Incorrect input data! Try again...");
+				}
+			}
+			userGuess = scanner.nextInt();
+			if (userGuess < minLimit ) {
+				System.out.println(String.format("The number can not be less than %s.", minLimit));
+			} else if(userGuess > maxLimit) {
+				System.out.println(String.format("The number can not be more than %s.", maxLimit));
+			} else {
+				isValid = true;
+			}
+		}
+		return userGuess;
+	}
+	
+	private boolean isValidSettings(int value, int lowerLimit, int upperLimit) {
+		boolean isValid = false;
+		if (value < lowerLimit ) {
+			System.out.println(String.format("The number can not be less than %s.", lowerLimit));
+		} else if(value > upperLimit) {
+			System.out.println(String.format("The number can not be more than %s.", upperLimit));
+		} else {
+			isValid = true;
+		}
+		return isValid;
+	}
+	
+	private void exit() {
+		System.out.println("Application is closed.");
+		System.exit(0);
+	}
 }
